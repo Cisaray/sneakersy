1f44cf1c43fef27093f3a56e3658526d41b0a6cf
Cisaray <lyadovkzst@gmail.com>
2023-03-24 05:54:21 +0500

    second day

---
+++ src/components/UnderHeader.js
@@ -0,0 +1,14 @@
+
+
+const UnderHeader=()=>{
+    return(
+        <div className="d-flex align-center mb-40 justify-between">
+            <h1>Все кроссовки</h1>
+            <div className="search-block d-flex">
+                <img src="/img/search.svg" alt="Search"/>
+                <input placeholder="Поиск..."/>
+            </div>
+        </div>
+    )
+}
+export default UnderHeader;
